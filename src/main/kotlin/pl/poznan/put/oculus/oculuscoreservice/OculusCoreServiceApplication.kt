23d0d46298055fc3fe95cab52815d80b6f9d0bdb
jakubriegel <jakubriegel112@gmail.com>
2019-11-27 19:49:17 +0100

    init

---
+++ src/main/kotlin/pl/poznan/put/oculus/oculuscoreservice/OculusCoreServiceApplication.kt
@@ -0,0 +1,11 @@
+package pl.poznan.put.oculus.oculuscoreservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OculusCoreServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<OculusCoreServiceApplication>(*args)
+}
---
+++ src/test/kotlin/pl/poznan/put/oculus/oculuscoreservice/OculusCoreServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.poznan.put.oculus.oculuscoreservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OculusCoreServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
