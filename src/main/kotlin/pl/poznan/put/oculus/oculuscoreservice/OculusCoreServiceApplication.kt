e70bfd54173027da4cda0e4de1317515b97e3cc5
jakubriegel <jakubriegel112@gmail.com>
2019-12-02 22:41:31 +0100

    facts from metrics

--- src/main/kotlin/pl/poznan/put/oculus/oculuscoreservice/OculusCoreServiceApplication.kt
+++ src/main/kotlin/pl/poznan/put/oculus/oculuscoreservice/OculusCoreServiceApplication.kt
@@ -2,8 +2,12 @@ package pl.poznan.put.oculus.oculuscoreservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@ComponentScan("pl.poznan.put.oculus")
+@EnableScheduling
 class OculusCoreServiceApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/pl/poznan/put/oculus/oculuscoreservice/service/JobsService.kt
+++ src/main/kotlin/pl/poznan/put/oculus/oculuscoreservice/service/JobsService.kt
@@ -1,18 +1,41 @@
 package pl.poznan.put.oculus.oculuscoreservice.service
 
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import pl.poznan.put.oculus.boot.exception.OculusException
 import pl.poznan.put.oculus.oculuscoreservice.model.Job
 import pl.poznan.put.oculus.oculuscoreservice.model.JobStatus
 import pl.poznan.put.oculus.oculuscoreservice.repository.JobsRepository
 
 @Service
 class JobsService (
-        private val repository: JobsRepository
+        private val repository: JobsRepository,
+        private val restTemplate: RestTemplate,
+        @Value("\${oculus.facts-service}")
+        private val factsServiceHost: String
 ) {
     fun createJob(job: Job) = repository.insert(job)
-            .also { logger.info("created new job with id ${it.id}") }
+            .also { logger.info("created new job ${it.id}") }
+            .apply { generateFacts() }
+
+    private fun Job.generateFacts() {
+        restTemplate.postForEntity(
+                "http://$factsServiceHost/facts/fromMetrics",
+                FactsFromMetricsRequest(patientMetrics, id!!),
+                Any::class.java
+        ).let {
+            when(it.statusCode) {
+                HttpStatus.CREATED -> Unit
+                else -> throw FactsFromMetricsCreationException()
+            }
+        }
+
+        logger.info("generated metrics facts for job $id")
+    }
 
     fun getJob(id: String) = repository.findByIdOrNull(id)
             .also { logger.info("getting job with id $id") }
@@ -21,6 +44,14 @@ class JobsService (
             .also { logger.info("updated status of job $jobId to $newStatus") }
 
     companion object {
+
+        private data class FactsFromMetricsRequest (
+                val patientMetricsId: String,
+                val jobId: String
+        )
+
+        private class FactsFromMetricsCreationException : OculusException("Error creating facts from metrics")
+        
         private val logger = LoggerFactory.getLogger(JobsService::class.java)
     }
 }
